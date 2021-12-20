10b7adf0067538ba8adb43e9fc2e97166c913abe
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-20 19:57:59 +0530

    Create compiler.js

---
+++ compiler.js
@@ -0,0 +1,9 @@
+export default function compile(input) {
+  input = input.replace(/###[^]*?###|#.*/gm, '')
+  let lines = input.split('\n')
+  let comment = false
+  let output = ''
+  for(let line of lines) {
+    
+  }
+}
