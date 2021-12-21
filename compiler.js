8bf458ca8c27c10dcb55c403df9111d15f8e3184
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-21 08:31:33 +0530

    Update compiler.js

--- compiler.js
+++ compiler.js
@@ -1,27 +1,26 @@
 function compile(input) {
-  input = input.replace(/###[^]*?###|#.*/gm, '')
-  let lines = input.split('\n')
-  let comment = false
-  let indents = []
-  let indent = 0
-  let output = ''
-  for(let line of lines) {
-    let statement = line.match(/^(\s*)(if|else|switch|try|catch|(?:async\s+)?function\*?|class|do|while|for)\s+(.+)/)
-    if(statement) {
-      let [, spaces, name, args] = statement
-      indents.unshift(spaces.length)
-      indent = spaces.length
-      output += `${spaces}${name} ${/function|try|class|/.test(name) ? args : `(${args})`} {\n`
+  input = input.replace(/###[^]*?###|#.*/gm, '');
+  let lines = input.split('\n');
+  let comment = false;
+  let indents = [];
+  let output = '';
+  for (let line of lines) {
+    let statement = line.match(
+      /^(\s*)(if|else|switch|try|catch|(?:async\s+)?function\*?|class|do|while|for)\s+(.+)/
+    );
+    if (statement) {
+      let [, spaces, name, args] = statement;
+      indents.unshift(spaces.length);
+      output += `${spaces}${name} ${/function|try|class/.test(name) ? args : `(${args})`} {\n`;
     } else {
-      let spaces = line.match(/^\s*/)[0].length
-      while(indent > spaces) {
-        let dedent = indents.shift()
-        indent -= dedent
-        output += `${' '.repeat(dedent)}}\n`
+      let spaces = line.match(/^\s*/)[0].length;
+      for (let indent of [...indents]) {
+        if (indent < spaces) break;
+        output += `${' '.repeat(indent)}}\n`;
+        indents.shift();
       }
-      indent = spaces
-      output += line + '\n'
+      output += line + '\n';
     }
   }
-  return output
+  return output;
 }
